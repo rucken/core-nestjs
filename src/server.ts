42eb614e6c09b3ff116f1b04a018aa22b788624f
endy <admin@site15.ru>
2018-01-17 00:59:36 +0500

    fix(cors):Add PUT for cors requests

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
 	app.use((req, res, next) => {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		next();
 	});
 	app.useGlobalFilters(new CustomExceptionFilter());
