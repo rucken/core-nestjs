efc0c53996dd6a8b98b8f21d656f69c4fd731aa4
endy <admin@site15.ru>
2018-01-17 20:51:50 +0500

    fix(filters): Add debug mode for all errors handlers

--- src/libs/core/exceptions/custom-exception.filter.ts
+++ src/libs/core/exceptions/custom-exception.filter.ts
@@ -6,9 +6,13 @@ import { CustomValidationError } from './custom-validation.error';
 
 @Catch(SyntaxError, CustomValidationError, JsonWebTokenError, Error)
 export class CustomExceptionFilter implements ExceptionFilter {
-    catch(exception: CustomValidationError | JsonWebTokenError, response) {
+    catch(exception: CustomValidationError | JsonWebTokenError | SyntaxError | Error, response) {
         const errors = {};
         if (exception instanceof CustomValidationError) {
+            if (process.env.DEBUG === 'true' || process.env.DEBUG === '1') {
+                response.status(HttpStatus.BAD_REQUEST).json(exception);
+                return;
+            }
             exception.errors.forEach((error: ValidationError) => {
                 Object.keys(error.constraints).forEach(
                     (key: string) => {
@@ -23,16 +27,24 @@ export class CustomExceptionFilter implements ExceptionFilter {
             return;
         }
         if (exception instanceof JsonWebTokenError) {
+            if (process.env.DEBUG === 'true' || process.env.DEBUG === '1') {
+                response.status(HttpStatus.BAD_REQUEST).json(exception);
+                return;
+            }
             response.status(HttpStatus.BAD_REQUEST).json({
-                nonFieldErrors: [exception.message]
+                message: exception.message
             });
             return;
         }
-        if (process.env.DEBUG === 'true' || process.env.DEBUG === '1') {
-            console.error(exception);
-            response.status(HttpStatus.BAD_REQUEST).json({ message: String(exception) });
+        if (exception instanceof SyntaxError || exception instanceof Error) {
+            if (process.env.DEBUG === 'true' || process.env.DEBUG === '1') {
+                response.status(HttpStatus.BAD_REQUEST).json(exception);
+                return;
+            }
+            response.status(HttpStatus.BAD_REQUEST).json({
+                message: exception.message ? exception.message : String(exception)
+            });
             return;
         }
-        response.status(HttpStatus.BAD_REQUEST).json(exception);
     }
 }
