eae96400f88146a27094faa1af20eb8d490a174d
endy <admin@site15.ru>
2018-01-17 20:54:10 +0500

    chore(server): Change default web server port to 5000

--- src/server.ts
+++ src/server.ts
@@ -52,6 +52,6 @@ async function bootstrap() {
 	*/
 	SwaggerModule.setup('/swagger', app, document);
 
-	await app.listen(process.env.PORT && !isNaN(+process.env.PORT) ? +process.env.PORT : 3000);
+	await app.listen(process.env.PORT && !isNaN(+process.env.PORT) ? +process.env.PORT : 5000);
 }
 bootstrap();
