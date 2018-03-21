25fb31848aef7014a758065efd96f0601a885cd6
endy <admin@site15.ru>
2018-03-21 20:57:46 +0500

    fix(custom.error): Remove duplicated variable

--- src/libs/core/exceptions/custom.error.ts
+++ src/libs/core/exceptions/custom.error.ts
@@ -2,9 +2,7 @@ import { ValidationError } from "class-validator";
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class CustomError extends Error {
-    error: string;
     constructor(error: string) {
-        super();
-        this.error = error;
+        super(error);
     }
 }
