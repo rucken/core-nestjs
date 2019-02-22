69ddf15d92c3a575d3e3dc7d875bc65086e75335
EndyKaufman <admin@site15.ru>
2019-02-22 17:13:21 +0500

    chore: Add scripts for deploy to VPS

---
+++ .travis/public-deploy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+setup_ssh() {
+    eval "$(ssh-agent -s)"
+    chmod 600 .travis/deploy.key
+    ssh-add .travis/deploy.key
+    ssh-keyscan ${REMOTE_HOST} >> $HOME/.ssh/known_hosts
+    ssh-keyscan ${REMOTE_HOST_IP} >> $HOME/.ssh/known_hosts
+}
+setup_git() {
+  git config user.email "travis@travis-ci.org"
+  git config user.name "Travis CI"
+  mkdir deploy
+  mkdir deploy/dist
+  cd deploy
+  git init
+  git remote add deploy "${REMOTE_HOST_GIT_URL}" > /dev/null 2>&1
+  git config push.default simple
+  git remote -v
+  git fetch deploy master
+  git pull deploy master
+  cd ..
+}
+
+commit_files() {
+  cp -av ./dist/* ./deploy/dist
+  cp -av ./package.json ./deploy/package.json 
+  cp -av ./.gitignore ./deploy/.gitignore
+  cp -av ./ormconfig.js ./deploy/ormconfig.js
+  cp -av ./README.md ./deploy/README.md
+  cd deploy
+  git add .
+  git commit --message "Version: $PACKAGE_VERSION Commit: $TRAVIS_COMMIT"
+  git push --quiet --set-upstream deploy master
+  cd ..
+}
+
+upload_files() {
+  rm -rf .git
+  cd deploy
+  git push deploy master
+  cd ..
+}
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+then
+  PACKAGE_VERSION=$(cat package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g')
+  export PACKAGE_VERSION=$PACKAGE_VERSION
+
+  setup_ssh
+  setup_git
+  commit_files
+  upload_files
+fi
---
+++ .travis/public-patch.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const f_gitignore = './.gitignore';
+const f_package_json = './package.json';
+
+var data = '', obj = {}, txt = '';
+
+data = fs.readFileSync(f_gitignore, 'utf8');
+txt = data.replace(/\/dist/g, '').replace(/\/client\/docs/g, '').replace(/\/client\/index.html/g, '');
+fs.writeFileSync(f_gitignore, txt, 'utf8');
+
+console.log('Updated .gitignore');
+
+data = fs.readFileSync(f_package_json, 'utf8');
+obj = JSON.parse(data);
+delete obj['devDependencies'];
+obj.scripts.build = 'exit 0';
+obj.scripts.test = 'exit 0';
+fs.writeFileSync(f_package_json, JSON.stringify(obj), 'utf8');
+
+console.log('Updated package.json');
