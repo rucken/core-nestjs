cef40b2905d7f7e1a3013416eca207135ea59753
EndyKaufman <admin@site15.ru>
2019-02-22 16:25:30 +0500

    chore: Update dependencies and support run migrations from vendors libs

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,20 @@
+const fg = require('fast-glob');
+const vendors = ['./dist/core', './dist/auth'];
 const ConnectionString = require('connection-string');
 const load = require('dotenv').load;
 const fs = require('fs');
-const NODE_ENV = process.env.NODE_ENV || 'develop';
-const sourceRootKey = NODE_ENV === 'develop' ? 'sourceRoot' : 'outputPath';
-const entitiesExt = NODE_ENV === 'develop' ? '{.ts,.js}' : '.js';
+const path = require('path');
+const envName = process.env.NODE_ENV || 'develop';
+const isDevelop = envName === 'develop';
+const sourceRootKey = isDevelop ? 'sourceRoot' : 'outputPath';
+const entitiesExt = isDevelop ? '{.js,.ts}' : '.js';
 const angularConfig = JSON.parse(fs.readFileSync('angular.json'));
 try {
-    fs.accessSync(`${NODE_ENV}.env`);
-    load({ path: `${NODE_ENV}.env` });
-    console.log(`env file: ${NODE_ENV}.env`);
+    fs.accessSync(`${envName}.env`);
+    load({ path: `${envName}.env` });
+    console.log(`env file: ${envName}.env`);
 } catch (error) {
-    console.log(`error on get env file: ${NODE_ENV}.env`);
+    console.log(`error on get env file: ${envName}.env`);
     try {
         fs.accessSync(`.env`);
         load();
@@ -21,22 +25,63 @@ try {
 }
 
 const connectionString = new ConnectionString(process.env.DATABASE_URL || '');
+const vendorsLibs = Object.keys(vendors).map(index => {
+    const vendorConfig = {};
+    vendorConfig[sourceRootKey] = vendors[index];
+    return vendorConfig;
+});
 const libs = Object.keys(angularConfig.projects).filter(key => angularConfig.projects[key].projectType === 'library').map(key => angularConfig.projects[key]);
 const apps = Object.keys(angularConfig.projects).filter(key => angularConfig.projects[key].projectType === 'application').map(key => angularConfig.projects[key]);
 const defaultProject = angularConfig.defaultProject;
 const defaultApp = angularConfig.projects[defaultProject];
 
+const entities = normalizationFileList(
+    fg.sync(
+        [
+            ...vendorsLibs,
+            ...libs,
+            ...apps
+        ].map(lib =>
+            `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/entities/**/*.entity${entitiesExt}`
+        )
+    )
+);
+const migrations = normalizationFileList(
+    fg.sync(
+        [
+            ...vendorsLibs,
+            ...libs,
+            ...apps
+        ].map(lib =>
+            `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/migrations/**/*${entitiesExt}`
+        )
+    )
+);
+const subscribers = normalizationFileList(
+    fg.sync(
+        [
+            ...vendorsLibs,
+            ...libs,
+            ...apps
+        ].map(lib =>
+            `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/subscribers/**/*${entitiesExt}`
+        )
+    )
+);
 if (connectionString.protocol === 'sqlite') {
     const dbFile =
         './' +
         (connectionString.hosts ? connectionString.hosts[0].name : '') +
         (connectionString.path ? '/' + connectionString.path[0] : '');
+    console.log(
+        [...vendorsLibs, ...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/migrations/**/*${entitiesExt}`)
+    );
     module.exports = {
         type: 'sqlite',
         database: dbFile,
-        entities: [...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/entities/**/*.entity${entitiesExt}`),
-        migrations: [...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/migrations/**/*${entitiesExt}`),
-        subscribers: [...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/subscribers/**/*${entitiesExt}`),
+        entities: entities,
+        migrations: migrations,
+        subscribers: subscribers,
         logging: 'all',
         synchronize: false,
         cli: {
@@ -51,13 +96,36 @@ if (connectionString.protocol === 'sqlite') {
         username: connectionString.user,
         password: connectionString.password,
         database: connectionString.path && connectionString.path[0],
-        entities: [...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/entities/**/*.entity${entitiesExt}`),
-        migrations: [...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/migrations/**/*${entitiesExt}`),
-        subscribers: [...libs, ...apps].map(lib => `${lib[sourceRootKey] || lib.architect.build.options[sourceRootKey]}/**/subscribers/**/*${entitiesExt}`),
+        entities: entities,
+        migrations: migrations,
+        subscribers: subscribers,
         logging: 'all',
         synchronize: false,
         cli: {
             migrationsDir: `${defaultApp[sourceRootKey]}/migrations`
         }
     }
+}
+
+function normalizationFileList(files) {
+    const newFiles = [];
+    for (var i = 0; i < files.length; i++) {
+        const filename = files[i];
+        if (filename.indexOf('.d.ts') === -1) {
+            var founded = false;
+            for (var j = 0; j < newFiles.length; j++) {
+                const filename = newFiles[j];
+                if (
+                    path.basename(filename, path.parse(filename).ext) ===
+                    path.basename(files[i], path.parse(files[i]).ext)
+                ) {
+                    founded = true;
+                }
+            }
+            if (!founded) {
+                newFiles.push(files[i]);
+            }
+        }
+    }
+    return newFiles;
 }
